b38e1afde50969545275780b08eae6e6c1bc07e2
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2023-02-16 09:32:40 +0100

    Simplify subclass

--- src/PasswordValidation/Validators/OneFailPasswordValidator.cs
+++ src/PasswordValidation/Validators/OneFailPasswordValidator.cs
@@ -4,22 +4,15 @@ namespace PasswordValidation.Validators;
 
 public class OneFailPasswordValidator : PasswordValidator
 {
-    public override List<string> Errors { get { return validator.Errors; } }
-    private PasswordValidator validator;
-
-    public OneFailPasswordValidator(string password) : base(password)
-        => validator = new PasswordValidator(password);
+   public OneFailPasswordValidator(string password) : base(password) {}
 
     public override bool IsValid()
     {
-        bool isValid = validator.IsValid();
+        bool isValid = base.IsValid();
 
         if (isValid)
             return true;
 
-        return validator.Errors.Count == 1;
+        return Errors.Count == 1;
     }
-
-    public override void AddCheck(PasswordCheck check)
-        => validator.AddCheck(check);
 }
